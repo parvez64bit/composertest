bb33ae4d26d2901e85c5426aaa8801e1384f7c8a
parvez64bit <parvez64bit@gmail.com>
2018-07-25 12:18:37 +0530

    Add existing file

---
+++ src/class.W3Ntrack.php
@@ -0,0 +1,15 @@
+<?php
+	class W3Ntrack {
+		/*
+		*
+		* Main Class where everthing start executing
+		*
+		*/
+		private $user_role;
+		private $user_type;
+
+		public __constructor() {
+			get_role();
+			set_role();
+		}
+	}
